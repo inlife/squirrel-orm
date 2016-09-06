a3b5713b960664d7370bf3960cd69fc95ddaf1d6
Vladislav Gritsenko <vladgritsenko@gmail.com>
2016-09-06 18:00:43 +0300

    updated structure for builder

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")/lib"
+cd "$(dirname "${BASH_SOURCE}")"
 
 cat\
-  $(sed -n 's/^__orm_include("\(.*\)".*/\1.nut/p' index.nut)\
-  > ../output.nut
+  $(sed -n 's/^__orm_include("\(.*\)".*/lib\/\1.nut/p' index.nut)\
+  > ../build.nut
